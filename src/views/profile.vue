afe22446347b623637595ea6597ddaea95800091
Serge I. Zolotukhin <serge.zolotukhin@gmail.com>
2019-03-11 11:58:58 +0300

    Template text removed from Profile.

--- src/views/profile.vue
+++ src/views/profile.vue
@@ -2,9 +2,11 @@
   <v-container>
     <v-layout>
       <v-flex>
+        <!--
         <h1>Profile</h1>
         <p>Отчет по каждому редактору и вообще total.</p>
         <v-divider></v-divider>
+        -->
         <p><v-btn depressed color="primary" @click="userSignOut">Выйти из системы</v-btn></p>
       </v-flex>
     </v-layout>
